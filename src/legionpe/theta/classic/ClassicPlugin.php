45b096da9c93780a35bd0ef52abed4ab3244a540
PEMapModder <pemapmodder1970@gmail.com>
2015-11-03 23:30:42 +0800

    Added fireballs

--- src/legionpe/theta/classic/ClassicPlugin.php
+++ src/legionpe/theta/classic/ClassicPlugin.php
@@ -58,6 +58,7 @@ class ClassicPlugin extends BasePlugin{
 //			new OneVsOneCommand($this),
 		]);
 		$this->getServer()->getScheduler()->scheduleDelayedTask(new BattleTask($this), 20);
+		new FireballTask($this);
 	}
 	public function getLoginQueryImpl(){
 		return ClassicLoginDataQuery::class;
